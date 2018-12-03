9fc24878250e32aa30e064a67c4a35067d7a1912
Byosphere <dreamlike.swarm@gmail.com>
2018-12-03 17:26:31 +0100

    ajout logo frame

--- src/components/frame/Frame.tsx
+++ src/components/frame/Frame.tsx
@@ -3,6 +3,7 @@ import './frame.css';
 import { IconButton } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import T from 'i18n-react';
+import logo from 'src/logo.png';
 
 interface Props { }
 interface State { }
@@ -21,6 +22,7 @@ class Frame extends React.Component<Props, State> {
     public render() {
         return (
             <div className="frame">
+                <img src={logo} alt="logo" style={{ marginLeft: "10px", height:"100%" }} />
                 <h1 className="app-title">{T.translate('appname')}</h1>
                 <IconButton onClick={() => { this.closeApp() }} color="secondary" aria-label="Close">
                     <Close />
